acc1e72bac6f0223b40d9ca0ce25a54f4621076d
Frank <imbruglia100@gmail.com>
2022-11-29 18:34:29 -0500

    Added new project and some new animations to project section

--- src/components/Projects/projectData.js
+++ src/components/Projects/projectData.js
@@ -1,25 +1,32 @@
 import travelImg from '../../assets/images/travel-advisor.png'
 import cryptoImg from '../../assets/images/crypto-media.png'
 import petImg from '../../assets/images/pet-adopt.png'
+import speedtyper from '../../assets/images/speed-typer.png'
 
 
 export const projects = [
-    {
-        title: 'Travel Advisor',
-        img: travelImg,
-        desc: 'I love to travel, so now I have a free way to find top rated restaurants, hotels and attractions where ever I go!',
-        url: 'https://travel-advisor-fji.netlify.app'
-    },
-    {
-        title: 'Crypto Media',
-        img: cryptoImg,
-        url: 'https://crypto-media.netlify.app',
-        desc: 'Crypto media shares up to date data of all coins, exhanges, and news.'
-    },
-    {
-        title: 'Pet Adoption',
-        img: petImg,
-        url: 'https://objective-elion-283150.netlify.app',
-        desc: 'Adopt a pal, or set one up for adoption!'
-    },
+  {
+    title: "Travel Advisor",
+    img: travelImg,
+    desc: "I love to travel, so now I have a free way to find top rated restaurants, hotels and attractions where ever I go!",
+    url: "https://travel-advisor-fji.netlify.app",
+  },
+  {
+    title: "Crypto Media",
+    img: cryptoImg,
+    url: "https://crypto-media.netlify.app",
+    desc: "Crypto media shares up to date data of all coins, exhanges, and news.",
+  },
+  {
+    title: "Measter of Keys",
+    img: speedtyper,
+    url: "https://storied-panda-9cbad4.netlify.app",
+    desc: "Type your way to the top. Practice typing speed and accuracy",
+  },
+  {
+    title: "Pet Adoption",
+    img: petImg,
+    url: "https://objective-elion-283150.netlify.app",
+    desc: "Adopt a pal, or set one up for adoption!",
+  },
 ];
